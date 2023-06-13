79e9009b6c6f5f90cb2941751b715e3e7085a616
Alessandro Alterno <28725795+lexinor@users.noreply.github.com>
2023-06-13 21:58:45 +0200

    Le bandage et le medikit soignent correctement

--- data/items.lua
+++ data/items.lua
@@ -1563,7 +1563,10 @@ return {
 			disable = { move = true, car = true, combat = true },
 			usetime = 8000,
 			notification = 'Vous avez utilisé un bandage !'
-		}
+		},
+		server = {
+			export = 'lexinor_hospital.bandage',
+		},
 	},
 
 	['stretcher'] = {
@@ -1580,11 +1583,14 @@ return {
 		description = "De quoi soigner des blessures plus importantes",
 	    client = {
 			anim = { dict = 'missheistdockssetup1clipboard@idle_a', clip = 'idle_a', flag = 49 },
-			prop = { model = `prop_rolled_sock_02`, pos = vec3(-0.14, -0.14, -0.08), rot = vec3(-50.0, -50.0, 0.0) },
+			prop = { model = `v_ret_ta_firstaid`, pos = vec3(0.0, 0.0, 0.0), rot = vec3(-50.0, -50.0, 0.0) },
 			disable = { move = true, car = true, combat = true },
 			usetime = 8000,
 			notification = 'Vous avez utilisé une Trousse de secours !'
-	    }
+	    },
+		server = {
+			export = 'lexinor_hospital.medikit',
+		},
 	},
 
 	['paracetamol'] = {
