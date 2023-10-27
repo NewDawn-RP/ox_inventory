d30932d46216e41acae9ebf84138d7ce85a1aefe
Dawsmo <139145295+Dawsmo@users.noreply.github.com>
2023-10-27 18:01:09 +0200

    Update coord evidence

--- data/evidence.lua
+++ data/evidence.lua
@@ -1,14 +1,14 @@
 return {
 	{
-		coords = vec3(448.8513, -997.4853, 30.6896),
+		coords = vec3(433.0106, -974.1216, 21.5595),
 		target = { -- qtarget support
 			name = 'rockfordhill_evidence', -- name of zone must be uniuqe
-			loc = vec3(448.8513, -997.4853, 30.6896),
+			loc = vec3(433.0106, -974.1216, 21.5595),
 			length = 5.0,
 			width = 3.0,
 			heading = 178.3122,
-			minZ = 29.09,
-			maxZ = 31.89,
+			minZ = 20.5595,
+			maxZ = 22.5595,
 			debug = false,
 		}
 	}
