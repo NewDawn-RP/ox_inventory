e775665f3936527ddb5704931ee1910d01dc5e7a
Lexinor <28725795+lexinor@users.noreply.github.com>
2023-11-18 17:59:29 +0100

    Ajout coffre preuve BCSO

--- data/evidence.lua
+++ data/evidence.lua
@@ -10,6 +10,19 @@ return {
 			minZ = 20.5595,
 			maxZ = 22.5595,
 			debug = false,
+		},
+	},
+    {
+		coords = vec3(1830.8716, 3679.8784, 38.8595),
+		target = { -- qtarget support
+			name = 'sandyshore_evidence', -- name of zone must be uniuqe
+			loc = vec3(1830.8716, 3679.8784, 38.8595),
+			length = 5.0,
+			width = 3.0,
+			heading = 178.3122,
+			minZ = 20.5595,
+			maxZ = 22.5595,
+			debug = false,
 		}
 	}
 }
